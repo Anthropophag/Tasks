ade0754fc86da1a7ac22d728985aaf0095b5b7c5
Anthropophag <anton.ingrish@gmail.com>
2022-05-22 07:47:42 +0300

    Решение 1 задачи Esey после "Полезных инструментов"
    (cherry picked from commit 9c4925e84f34dc1da86fc7e5be9889c519bb361a)
    
    # Conflicts:
    #	save.py

--- save.py
+++ save.py
@@ -1,14 +1,42 @@
-fruit_basket_1 = ['яблоко', 'груша', 'маракуя', "банан"]
-fruit_basket_2 = ['личи', 'арбуз', "банан", 'маракуя']
-fruit_basket_general = []
-
-for fruit in fruit_basket_1:
-    if fruit in fruit_basket_2:
-        fruit_basket_general.append(fruit)
-print(fruit_basket_general)
+from random import randint
 
 
+def attack(person1, person2):
+    lucky_coin = randint(0, 1)
+    while person1['health'] > 0 or person2['health'] > 0:
+        if lucky_coin == 0:
+            person1['health'] -= person2['damage']
+        else:
+            person2['health'] -= person1['damage']
+    if person1['health'] > 0:
+        print('WINER - ', person1['name'])
+        print('health - ', person1['health'])
+    else:
+        print('WINER - ', person2['name'])
+        print('health - ', person2['health'])
 
+
+def harm(damege, armor):
+    return damege / armor
+
+
+name = input()
+
+with open('name_enemy.txt', mode='r', encoding='utf-8') as enemy_inf:
+    enemy_inf = enemy_inf.readlines()
+enemy = {}
+for parm in enemy_inf:
+    inf = parm.strip().split()
+    enemy[inf[0]] = inf[1]
+print(enemy)
+
+with open('name_hero.txt', mode='r', encoding='utf-8') as hero_inf:
+    hero_inf = hero_inf.readlines()
+hero = {}
+for parm in hero_inf:
+    inf = parm.strip().split()
+    hero[inf[0]] = inf[1]
+print(hero)
 
 
 
