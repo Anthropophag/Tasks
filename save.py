9c4925e84f34dc1da86fc7e5be9889c519bb361a
Anthropophag <anton.ingrish@gmail.com>
2022-05-22 07:47:42 +0300

    Решение 1 задачи Esey после "Полезных инструментов"

--- save.py
+++ save.py
@@ -1,42 +1,23 @@
-from random import randint
+import random
+
+numbers = []
+for i in range(random.randint(0, 1000)):
+    numbers.append(random.randint(0, 1000000))
+
+for ind in range(len(numbers)):
+    numbers[ind] = numbers[ind] ** 2
+print(numbers)
 
 
-def attack(person1, person2):
-    lucky_coin = randint(0, 1)
-    while person1['health'] > 0 or person2['health'] > 0:
-        if lucky_coin == 0:
-            person1['health'] -= person2['damage']
-        else:
-            person2['health'] -= person1['damage']
-    if person1['health'] > 0:
-        print('WINER - ', person1['name'])
-        print('health - ', person1['health'])
-    else:
-        print('WINER - ', person2['name'])
-        print('health - ', person2['health'])
 
 
-def harm(damege, armor):
-    return damege / armor
 
 
-name = input()
 
-with open('name_enemy.txt', mode='r', encoding='utf-8') as enemy_inf:
-    enemy_inf = enemy_inf.readlines()
-enemy = {}
-for parm in enemy_inf:
-    inf = parm.strip().split()
-    enemy[inf[0]] = inf[1]
-print(enemy)
 
-with open('name_hero.txt', mode='r', encoding='utf-8') as hero_inf:
-    hero_inf = hero_inf.readlines()
-hero = {}
-for parm in hero_inf:
-    inf = parm.strip().split()
-    hero[inf[0]] = inf[1]
-print(hero)
+
+
+
 
 
 
--- ОГЭ.py
+++ ОГЭ.py
@@ -1,11 +1,7 @@
 with open('input.txt', mode='r') as test:
-    str = test.readlines().strip().split('; ')
+    st = test.readlines().strip().split('; ')
 for _ in range(9)
     if (s > 10) or (t > a):
         print("YES")
     else:
         print("NO")
-
-
-
-
