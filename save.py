0e594346e4916976dcff61746d483284b77dd4cb
Anthropophag <anton.ingrish@gmail.com>
2022-05-25 08:02:59 +0300

    lesson 4 medium(1) Как сделать красивые принты

--- save.py
+++ save.py
@@ -1,24 +1,25 @@
-import random
-
-numbers = [random.randint(0, 1000) for i in range(random.randint(0, 1000))]
-new_numbers = [i for i in numbers
-               if i % 3 == 0 and
-               i > 0 and
-               i % 4 != 0]
-print(numbers)
-print(new_numbers)
+import re
 
 
+def registration(NL, email):
+    patern_name = '[A-Z][a-z]+ [A-Z][a-z]+'
+    patern_email = '[a-z0-9_\.]+@[a-z0-9]+\.[com|org|ru]+'
+    count = 0
+    while count == 0:
+        if re.match(patern_name, NL) is None:
+            NL = input('Имия или фомилия введены неверно, повторите попытку: ')
+        else:
+            print('Имя и фамилия введены верно')
+            count += 1
+    count = 0
+    while count == 0:
+        if re.match(patern_email, email) is None:
+            email = input('email введён не правлиьлно, повторите попытку: ')
+        else:
+            print('email введён верно')
+            count += 1
 
 
-
-
-
-
-
-
-
-
-
-
-
+NL = input()
+email = input()
+registration(NL, email)
