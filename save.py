6e6d069810b9da9f3f7dbf172b1e538b06fb7e6a
Anthropophag <anton.ingrish@gmail.com>
2022-05-28 11:10:08 +0300

    lesson 5 easy(3) Создание копии файла

--- save.py
+++ save.py
@@ -1,10 +1,2 @@
-def copy_this_file():
-    filename = __file__
-    with open(filename, mode='r') as first:
-        name, extension = filename.split('.')
-        with open(name + '_copy.' + extension, mode='w', encoding='utf-8') as dublicate:
-            print(f'{first.read()}', file=dublicate)
-
-
-if __name__ == '__main__':
-    copy_this_file()
+def plus(a, b):
+    return a + b
