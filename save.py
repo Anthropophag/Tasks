02dcaccabab7cd223343a4c742d19e1187081d96
Anthropophag <anton.ingrish@gmail.com>
2022-05-28 15:29:33 +0300

    lesson 5 normal(1) Консольная утилита для управления файловой системой

--- save.py
+++ save.py
@@ -1,10 +1,65 @@
-def copy_this_file():
-    filename = __file__
-    with open(filename, mode='r') as first:
-        name, extension = filename.split('.')
-        with open(name + '_copy.' + extension, mode='w', encoding='utf-8') as dublicate:
-            print(f'{first.read()}', file=dublicate)
+import os
 
 
-if __name__ == '__main__':
-    copy_this_file()
+def make_dir(patch):
+    try:
+        os.mkdir(patch)
+        print('Директория успешно создана')
+    except FileExistsError:
+        print('Директория уже была создана')
+    except PermissionError:
+        print('Недостаточно прав для создания директории')
+
+
+def remove_dir(patch):
+    try:
+        os.removedirs(patch)
+        print('Директория успешно удалена')
+    except FileNotFoundError:
+        print('Директория не найдена')
+    except PermissionError:
+        print('Недостаточно прав для создания директории')
+
+
+def list_dir():
+    print(*[i for i in os.listdir() if os.path.isdir(i)])
+
+
+def chenge_dir(patch):
+    try:
+        os.chdir(patch)
+        print('Вы успешно перешли в директорию')
+    except FileNotFoundError:
+        print('Заданной папки не существует')
+
+
+do = {1: chenge_dir,
+      2: list_dir,
+      3: remove_dir,
+      4: make_dir}
+
+while True:
+    choice = input('Выберите пункт:\n'
+                   '1. Перейти в папку\n'
+                   '2. Посмотреть содержимое текущей папки\n'
+                   '3. Удалить папку\n'
+                   '4. Создать папку\n'
+                   '5. Выйти\n'
+                   '---------------------\n'
+                   'Ваш выбор: ')
+    try:
+        if len(choice.split()) == 2:
+            choice, foldername = choice.split()
+            choice = int(choice)
+            if do.get(choice):
+                do[choice](foldername)
+        else:
+            choice = int(choice)
+            if choice == 5:
+                break
+            if do.get(choice):
+                print(do[choice]())
+    except ValueError:
+        print('Вы ввели неверные данные')
+    except TypeError:
+        print('Вы не указали имя папки')
--- test.py
+++ test.py
@@ -1,8 +1,5 @@
-with open('output.txt', mode='w') as test:
-    str = test.readlines().strip().split('; ')
-    for test_znach in str:
-        print(test_znach[1:len(test_znach) - 1].split(','))
-
+import os
+os.mkdir('chomic')
 
 
 
