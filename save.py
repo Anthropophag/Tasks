6811ccec851eaee3f5f7f3508012cc78afa00c16
Anthropophag <anton.ingrish@gmail.com>
2022-05-30 11:12:08 +0300

    lesson 6 normal(1) Как сделать реализацию цикла через классы

--- save.py
+++ save.py
@@ -1,46 +1,52 @@
-class Car:
-    def __init__(self, speed, color, name, is_police: bool):
-        self.speed = speed
-        self.color = color
+class Person:
+    health: int
+
+    def __init__(self):
+        self.health = 150
+        self.armor = 0
+        self.demage = 0
+
+    def _clean_damage(self):
+        return self.armor - self.demage
+
+    def attack(self, name_person):
+        if person.armor > 0:
+            uron = self._clean_damage()
+            person.armor -= uron
+            print(f'You dealt {uron} damage to an {name_person.name}')
+        else:
+            person.health += person.armor
+        if person.health > 0:
+            uron = self._clean_damage()
+            person.health -= uron
+            print(f'You dealt {uron} damage to an {name_person.name}')
+        else:
+            print(f'{person.name} defeated')
+
+
+class Player(Person):
+    def __init__(self, name: str):
+        super().__init__()
         self.name = name
-        self.is_police = is_police
+        self.demage = 15
+        self.armor = 30
+        print(f'Created player {name}')
 
-    def go(self):
-        print(f'{self.name} driving')
-
-    def stop(self):
-        print(f'{self.name} stoped')
-
-    def turn(self, direction):
-        print(f'{self.name} turned {direction}')
+    demage = 15
+    armor = 30
 
 
-class TownCar(Car):
-    def __init__(self, speed, color, name, is_police: bool):
-        super().__init__(speed, color, name, is_police)
-        if speed > 100:
-            print('Car not for town')
+class Enemy(Person):
+    def __init__(self):
+        super().__init__()
+        self.name = 'Orc'
+    demage = 25
+    armor = 10
 
 
-class SportCar(Car):
-    def __init__(self, speed, color, name, is_police: bool):
-        super().__init__(speed, color, name, is_police)
-        if speed < 100:
-            print('Car not for race')
+player1 = Player('Vonerut')
+person = Enemy()
 
+player1.attack(person)
 
-class WorkCar(Car):
-    def __init__(self, speed, color, name, is_police: bool):
-        super().__init__(speed, color, name, is_police)
-        print('Remember that this car must be returned in good condition')
-
-
-class PoliceCar(Car):
-    def __init__(self, speed, color, name, is_police: bool):
-        super().__init__(speed, color, name, is_police)
-
-    def siren(self, condition: str):
-        if condition == 'on':
-            print('Siren on !')
-        if condition == 'off':
-            print('Siren off.')
+print(person.health)
