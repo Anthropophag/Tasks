00e1e98e23b0f9c3d6e341984cc6b56d6b0ba392
Anthropophag <anton.ingrish@gmail.com>
2022-06-03 10:38:03 +0300

    lesson 7 (loto) Как сделать зачёркивание элементов и как осуществить ходы компьютера

--- test.py
+++ test.py
@@ -1,12 +1,9 @@
-import os
-os.mkdir('chomic')
-
-
-
-
-
-
-
-
-
-
+qwe = [
+    ['sg', 'n'],
+    ['dfsg', 3],
+    [45, 2, 1.5],
+    [12, 4, 456]
+]
+if __name__ == '__main__':
+    for ind, el in enumerate(qwe):
+        print(ind, el)
