64a3bd804cd7423551f311c68db08d96ae0300a3
Anthropophag <anton.ingrish@gmail.com>
2022-06-08 09:20:02 +0300

    loto полностью сделал ход игрока осталось сделать ход компьютера

--- save.py
+++ save.py
@@ -10,7 +10,6 @@ class Player:
     def _generate_index(self):
         random_index = []
         for _ in range(5):
-            metil = 0
             while True:
                 random_num = randint(0, 8)
                 if random_num not in random_index:
@@ -30,32 +29,46 @@ class Player:
                         break
         return card
 
-
-def start_play(self):
-    player_card = self._generate_card()
-    computer_card = self._generate_card()
-    barrels = [keg for keg in range(1, 91)]
-    ind = 89
-
-    random_keg = barrels[randint(0, ind)]
-    print(f'Your got keg {random_keg}')
-    print('-------Your card-------')
-    for i in player_card:
-        print(*i)
-    print('-----------------------')
-    print('-----Computer card-----')
-    for i in computer_card:
-        print(*i)
-    print('-----------------------')
-    choice = input('Cross out a number?\n'
-                   '    y / n\n'
-                   'Your choice: ')
-    match choice:
-        case 'y':
-            if random_keg in player_card:
-
+    def start_play(self):
+        player_card = self._generate_card()
+        computer_card = self._generate_card()
+        ind = 89
+        on_of = 0
+        use_keg = []
+        while on_of == 0:
+            random_keg = randint(1, 90)
+            if random_keg in use_keg:
+                continue
+            else:
+                print(f'Your got keg {random_keg}')
+                print('-------Your card-------')
+                for i in player_card:
+                    print(*i)
+                print('-----------------------')
+                print('-----Computer card-----')
+                for i in computer_card:
+                    print(*i)
+                print('-----------------------')
+                choice = input('Cross out a number?\n'
+                               '    y / n\n'
+                               'Your choice: ')
+                for ind, nums in enumerate(player_card):
+                    if random_keg in nums:
+                        if choice == 'y':
+                            player_card[ind][nums.index(random_keg)] = '-'
+                            use_keg.append(random_keg)
+                        else:
+                            print('You lose :(')
+                            on_of += 1
+                    if random_keg not in nums:
+                        if choice == 'y':
+                            print('You lose :(')
+                            on_of += 1
+                        else:
+                            on_of += 0
+                print('Сomputer move...')
 
 
 if __name__ == '__main__':
     chel = Player('wrtwt', 34, 'sgf')
-    start_play(chel)
+    chel.start_play()
--- test.py
+++ test.py
@@ -1,9 +1,3 @@
-qwe = [
-    ['sg', 'n'],
-    ['dfsg', 3],
-    [45, 2, 1.5],
-    [12, 4, 456]
-]
-if __name__ == '__main__':
-    for ind, el in enumerate(qwe):
-        print(ind, el)
+import os
+
+os.mkdir('chomic')
